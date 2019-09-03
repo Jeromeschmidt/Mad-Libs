d3cee58739c02b08f25accb674bf4afeab57bfc3
jeromeschmidt <schmidt.jerome@yahoo.com>
2019-09-02 20:43:30 -0700

    working mad libs program

--- mad_libs/mad-libs.py
+++ mad_libs/mad-libs.py
@@ -15,21 +15,33 @@
 # from PyDictionary import PyDictionary
 #
 # dictionary = PyDictionary()
+import random
 
 noun = list()
-plural_noun = list()
 adjective = list()
 verb = list()
-interjection = list()
-preposition = list()
 adverb = list()
-sampleMadLib = "\nLittle Red Riding <nn  is a/an <jj fairy tale for young children.\nIt is a story about a/an <jj girl and a wolf.\nThe girl's mother sends her to take <food to her sick grandmother.\nThe mother tells her she must not <vb on the way.\nA wolf sees the girl walking through the <nns and makes a plan to <vb her.\nThe wolf <rb asks the girl where she is going.\nThe girl <vbz him, because he seems <jj.\nThen the wolf tells her to pick some <nns for her grandmother.\nWhile she is <vbg flowers, the wolf goes to her grandmother's house and eats her.\nHe puts on the grandmother's <nn and gets into her bed.\nWhen the girl arrives at her grandmother's house, she gets into <nn with the wolf.\nThe wolf leaps upon the child and <vbz her"
+sampleMadLib = "\nLittle Red Riding <nn is a \n \
+<jj fairy tale for young children. \n \
+It is a story about a <jj girl and a wolf. \n \
+The girl's mother sends her to take <nn to her sick grandmother. \n \
+The mother tells her she must not <vb on the way. \n \
+A wolf sees the girl walking through the <nn (plural) and makes a plan to <vb her. \n \
+The wolf <rb asks the girl where she is going. \n \
+The girl <vb (plural) him, because he seems <jj . \n \
+Then the wolf tells her to pick some <nn (plural) for her grandmother. \n \
+While she is <vb (plural) flowers, the wolf goes to her grandmother's house and eats her. \n \
+He puts on the grandmother's <nn and gets into her bed. \n \
+When the girl arrives at her grandmother's house, she gets into <nn with the wolf. \n \
+The wolf leaps upon the child and <vb (plural) her"
 
 def user_input(prompt):
     #taken from Captain Rainbow checklist#
     # the input function will display a message in the terminal
     # and wait for user input.
     user_input = input(prompt)
+    if(user_input.isalpha() == False):
+        user_input("please enter a valid input:")
     return user_input
 
 def getNoun():
@@ -44,6 +56,9 @@ def getAdjective():
 def getVerb():
     verb.append(user_input("Enter a verb:\n"))
 
+def getPlural_Verb():
+    plural_verb.append(user_input("Enter a plural verb:\n"))
+
 def getInterjection():
     interjection.append(user_input("Enter a interjection:\n"))
 
@@ -54,44 +69,44 @@ def getAdverb():
     adverb.append(user_input("Enter a adverb:\n"))
 
 def fillInMadLib():
-    for i in range(len(sampleMadLib)):
-        if(sampleMadLib[i-1] == "<"):
+    i = 0
+    j = 0
+    final = ""
+    while(i < len(sampleMadLib)):
+        if(sampleMadLib[i] == "<"):
             j = i
             while(sampleMadLib[j] != " "):
-                j += 1
-            sampleMadLib.split(i-1, j) = noun[1]
-    for i in range(len(sampleMadLib)):
-        print(sampleMadLib[i])
+                j+=1
+            if(sampleMadLib[i:j] == "<nn"):
+                final = final + noun[random.randint(0, len(noun)-1)]
+            elif(sampleMadLib[i:j] == "<jj"):
+                final = final + adjective[random.randint(0, len(adjective)-1)]
+            elif(sampleMadLib[i:j] == "<vb"):
+                final = final + verb[random.randint(0, len(verb)-1)]
+            elif(sampleMadLib[i:j] == "<rb"):
+                final = final + adverb[random.randint(0, len(adverb)-1)]
 
-def test():
-    for i in range(len(sampleMadLib)):
-        if(sampleMadLib[i-1] == "<"):
-            compString = ((sampleMadLib[i] + sampleMadLib[i+1] +sampleMadLib[i+2]))
-            if(compString == "nn "):
-                getNoun()
-            elif(compString == "jj "):
-                getAdjective()
-            elif(compString == "vb "):
-                getVerb()
-            elif(compString == "nns "):
-                getPlural_Noun()
-            elif(compString == "rb "):
-                getAdverb()
-            elif(compString == "vbg "):
-                getNoun()
-            elif(compString == "vbz "):
-                getNoun()
-            else:
-                getNoun()
+            i = j
+        else:
+            final = final + sampleMadLib[i]
+            i += 1
+    print(final)
 
-            # if( == "nn "):
-            #     print((sampleMadLib[i] + sampleMadLib[i+1] +sampleMadLib[i+2]))
-            #     getNoun()
+def initial_start():
+    runWhile = True
+    while(runWhile == True):
+        for i in range(len(sampleMadLib)):
+            if(sampleMadLib[i-1] == "<"):
+                compString = ((sampleMadLib[i] + sampleMadLib[i+1] +sampleMadLib[i+2]))
+                if(compString == "nn "):
+                    getNoun()
+                elif(compString == "jj "):
+                    getAdjective()
+                elif(compString == "vb "):
+                    getVerb()
+                else:#(compString == "rb "):
+                    getAdverb()
 
-            #elif():
-
-            #else:
-    fillInMadLib()
-
-
-test()
+        runWhile = False
+        fillInMadLib()
+initial_start()
